0a871d3659e62dd9f5b296f1839621ad8b4b7806
CorvusYe <CorvusY@foxmail.com>
2022-08-17 12:34:23 +0800

    add docs for more details

--- src/components/MdDoc.vue
+++ src/components/MdDoc.vue
@@ -3,6 +3,7 @@ import { lang } from '../language'
 import { defineComponent, ref } from "vue";
 import { useRoute } from 'vue-router'
 import axios from "axios"
+import { ElLoading } from 'element-plus'
 
 let route = useRoute()
 
@@ -17,12 +18,16 @@ defineComponent({
 })
 
 async function getMdText(path: String, file: String) {
-  const res = await axios({
-    method: 'get',
-    url: `./docs/${lang}/md/${path}/${file}.md`
-  })
-  mdContent.value = await res.data
-  mdShow.value = true
+  const loadingInstance1 = ElLoading.service({ fullscreen: false, target: '.j_doc' })
+  try {
+    const res = await axios({
+      method: 'get',
+      url: `./docs/${lang}/md/${path}/${file}.md`
+    })
+    mdContent.value = await res.data
+    mdShow.value = true
+  } catch (e) {}
+  loadingInstance1.close()
 }
 
 getMdText(route.query.path as string, route.query.file as string)
@@ -30,12 +35,14 @@ getMdText(route.query.path as string, route.query.file as string)
 </script>
 
 <template>
-  <v-md-preview v-if="mdShow" :text="mdContent" class="md"></v-md-preview>
+  <div class="j_doc">
+    <v-md-preview v-if="mdShow" :text="mdContent" class="md"></v-md-preview>
+  </div>
 </template>
 
 <style scoped>
 .md {
-  max-width: 1080px;
+  max-width: 1180px;
   margin: 0 auto;
   width: 100%;
 }
